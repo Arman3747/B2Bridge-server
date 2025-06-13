69114b42be4d28b77340287b988010b9b3465877
Arman3747 <dailyarman7@gmail.com>
2025-06-13 23:17:55 +1000

    delete api created

--- index.js
+++ index.js
@@ -90,13 +90,13 @@ async function run() {
     //   res.send(result);
     // })
 
-    // //Delete
-    // app.delete('/allproducts/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = await productsCollection.deleteOne(query);
-    //   res.send(result);
-    // })
+    //Delete
+    app.delete('/allproducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
